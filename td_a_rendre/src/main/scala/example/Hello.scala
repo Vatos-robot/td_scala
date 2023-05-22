47edf37db74e8f61f28e226ec443ca0f3f2066e7
hugobarbosa <hugodu38@icloud.com>
2023-05-22 18:29:31 +0200

    affiche egalement le nombre de ligne du fichier csv

--- td_a_rendre/src/main/scala/example/Hello.scala
+++ td_a_rendre/src/main/scala/example/Hello.scala
@@ -1,12 +1,15 @@
 import scala.io.Source
 
-object Main extends App {
-  if (args.length > 0) {
-    val filename = args(0)
-    val content = Source.fromFile(filename).getLines().toVector
-    // afficher le contenue du csv 
-    println(content)
-  } else {
-    println("Erreur : Le programme attend au moins un argument")
+object Main {
+  def main(args: Array[String]): Unit = {
+    if (args.length >= 1) {
+      val filename = args(0)
+      val lines = Source.fromFile(filename).getLines().toVector
+      println("Contenu du fichier :")
+      lines.foreach(println)
+      println(s"Nombre de lignes : ${lines.length}")
+    } else {
+      println("Erreur : Aucun fichier spécifié.")
+    }
   }
 }
